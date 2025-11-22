74f0b020429a2baaad8a1db627f0941c1b8fa7d5
Zamiul_Hasan <zamiulhasan6@gmail.com>
2025-11-22 16:34:47 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
- * @author NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
- * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ * @author MD ZAMIUL HASAN
+ * ! The source code is written by MD ZAMIUL HASAN, please don't change the author's name everywhere. Thank you for using
  * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
  */
 
